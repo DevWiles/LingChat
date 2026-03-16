01cd530b3e8528befce18d8bb594e7f6401546bf
Wiles <nathan.demon66@gmail.com>
2026-03-17 01:36:07 +0800

    修改了配置文件，将框架改回springboot

--- lingchat-gateway/src/main/java/org/lingchat/lingchatgataway/LingchatGatewayApplication.java
+++ lingchat-gateway/src/main/java/org/lingchat/lingchatgataway/LingchatGatewayApplication.java
@@ -4,10 +4,10 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class LingchatGatawayApplication {
+public class LingchatGatewayApplication {
 
     public static void main(String[] args) {
-        SpringApplication.run(LingchatGatawayApplication.class, args);
+        SpringApplication.run(LingchatGatewayApplication.class, args);
     }
 
 }
